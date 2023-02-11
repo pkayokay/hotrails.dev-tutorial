725d123ac6f0589c32e15ace79f1e57f94a605a8
Paul Kim <pkim@hey.com>
2023-02-11 17:50:24 -0500

    Simply with naming convention

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,5 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quote_stream", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_to "quotes" }
 end
