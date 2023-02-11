bf68843b9a4299558247639c002923c1e8f87549
Paul Kim <pkim@hey.com>
2023-02-11 15:12:06 -0500

    Add turbo stream for creation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "quote_stream", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 end
