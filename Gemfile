cd4f03469bc41a4983c6ee7e6ef72d5af6d0d1d6
Paul Kim <pkim@hey.com>
2023-02-13 21:01:06 -0500

    Setup user, company and quotes associations and seeds

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ group :test do
 end
 
 gem "cssbundling-rails", "~> 1.1"
+
+gem "devise", "~> 4.8"
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :validatable
+  belongs_to :company
+end
